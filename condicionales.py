4729ab44aa1053d3e47a0569055938be56b787ed
Hernán D. Mercado <hernandmf@gmail.com>
2022-08-10 22:26:42 -0500

    conditionals with python

---
+++ condicionales.py
@@ -0,0 +1,17 @@
+# Condicionales sencillas
+
+edad = int(input("Escribe tu edad: "))
+if edad > 17:
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
+
+
+numero = int(input("Escribe un número: "))
+
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a 5")
+else: 
+    print("Es menor a 5")
