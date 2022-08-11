6aca7effe99887990cbeb43b01699e112c086586
Hernán D. Mercado <hernandmf@gmail.com>
2022-08-11 09:30:53 -0500

    string methods

--- funciones.py
+++ funciones.py
@@ -8,18 +8,26 @@
 # imprimir_mensaje()
 # imprimir_mensaje()
 
-def conversacion(mensaje):
-    print("Hola")
-    print("Como estás")
-    print(mensaje)
-    print("adios")
+# def conversacion(mensaje):
+#     print("Hola")
+#     print("Como estás")
+#     print(mensaje)
+#     print("adios")
 
-opcion = int(input("Elige una opción (1, 2, 3): "))
-if opcion == 1:
-    conversacion("Elegiste la opción 1")
-elif opcion == 2:
-    conversacion("Elegiste la opción 2")
-elif opcion == 3:
-    conversacion("Elegiste la opción 3")
-else:
-    print("Escribe la opción correcta")
+# opcion = int(input("Elige una opción (1, 2, 3): "))
+# if opcion == 1:
+#     conversacion("Elegiste la opción 1")
+# elif opcion == 2:
+#     conversacion("Elegiste la opción 2")
+# elif opcion == 3:
+#     conversacion("Elegiste la opción 3")
+# else:
+#     print("Escribe la opción correcta")
+
+
+def sumar(a, b):
+    suma = a + b
+    return suma 
+
+primer = sumar(2, 7)
+print(primer)
---
+++ stringmethonds.py
@@ -0,0 +1,10 @@
+nombre = "hernan "
+
+nombre.upper()
+print(nombre.upper())
+print(nombre.capitalize())
+print(nombre.strip())
+print(nombre.lower())
+print(nombre.replace("e", "a"))
+print(nombre[0])
+print(len(nombre))
