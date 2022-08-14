2e62fcc5f293d28c855e54bcf9cf96fd7a3c200a
Hernán D. Mercado <hernandmf@gmail.com>
2022-08-14 15:22:44 -0500

    manipulating dictionaries

--- diccionarios.py
+++ diccionarios.py
@@ -1,9 +1,18 @@
 def run():
     mi_diccionario = {
+        "Jaime": 2,
         "llave1": 1,
         "llave2": 2,
         "llave3": 3,
     }
+
+    print(mi_diccionario.get("Juan", "No existe el nombre de juan"))
+    mi_diccionario["Hernan"] = 70
+
+
+    del mi_diccionario["Jaime"]
+
+    print("Jaime" in mi_diccionario)
 
     print(mi_diccionario["llave1"])
     print(mi_diccionario["llave2"])
