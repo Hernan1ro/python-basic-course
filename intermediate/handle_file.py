ec6b90b7230c327f5e88cfe11a3211bd19a78d4f
Hernán D. Mercado <hernandmf@gmail.com>
2022-08-18 20:00:36 -0500

    work with files in python

---
+++ intermediate/handle_file.py
@@ -0,0 +1,24 @@
+def read():
+    numbers = []
+    with open("./files/numbers.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    print(numbers)
+
+
+def write():
+    names = ["Hernan", "Facundo", "Pepe", "Freddy"]
+    with open("./files/names.txt", "a", encoding="utf8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+
+
+
+def run():
+    write()
+
+
+
+if __name__ == '__main__':
+    run()
